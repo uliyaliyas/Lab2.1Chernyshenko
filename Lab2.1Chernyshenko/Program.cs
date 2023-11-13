f9951b789d1449e03fc1eae0a3fe79c0a41f3b82
uliyalyaaas <uliyaliya.1304@gmail.com>
2023-11-13 15:53:05 +0200

    Добавьте файлы проекта.

---
+++ Lab2.1Chernyshenko/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите k:");
+int k = int.Parse(Console.ReadLine());
+Console.Write("Введите l:");
+int l = int.Parse(Console.ReadLine());
+Console.Write("Введите n:");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите m:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine((n > 1 || m <= l + k) && (!(n > 2) || (Math.Pow(m, 2) > Math.Pow(l, 2))));
